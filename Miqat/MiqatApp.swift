90e79b017002e206ea7af7f6845af75a4f6fe86a
Azhar Ali <azharbinanwar@gmail.com>
2026-06-18 02:16:52 +0500

    Initial Commit

---
+++ Miqat/MiqatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiqatApp.swift
+//  Miqat
+//
+//  Created by Azhar Ali on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MiqatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
