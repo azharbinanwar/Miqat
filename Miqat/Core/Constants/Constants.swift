30f8698fbc7b51855704742973dcac2efa862bfd
Azhar Ali <azharbinanwar@gmail.com>
2026-06-28 03:16:09 +0500

    fix: IPrayedButton displayDate, bump v1.0.1, update UserDefaults keys to dev.miqat namespace

--- Miqat/Core/Constants/Constants.swift
+++ Miqat/Core/Constants/Constants.swift
@@ -2,13 +2,13 @@ import Foundation
 
 enum Keys {
     enum Defaults {
-        static let locations        = "dev.miqat.locations.v2"
-        static let activeLocationId  = "dev.miqat.activeLocationId"
-        static let settings         = "dev.miqat.settings.v4"
-        static let accentColorIndex = "dev.miqat.accentColorIndex"
-        static let selectedMadhab          = "dev.miqat.selectedMadhab"
-        static let hasCompletedOnboarding  = "dev.miqat.hasCompletedOnboarding"
-        static let floatingPanelX          = "dev.miqat.floatingPanelX"
-        static let floatingPanelY          = "dev.miqat.floatingPanelY"
+        static let locations        = "miqat.locations"
+        static let activeLocationId  = "miqat.default.location.id"
+        static let settings         = "miqat.settings"
+        static let accentColorIndex = "miqat.accent.color"
+        static let selectedMadhab          = "miqat.madhab"
+        static let hasCompletedOnboarding  = "miqat.show.onboarding"
+        static let floatingPanelX          = "miqat.floatingPanelX"
+        static let floatingPanelY          = "miqat.floatingPanelY"
     }
 }
